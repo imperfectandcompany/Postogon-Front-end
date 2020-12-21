2253dfe888530adc2d99366c9bf51fc53ff2da10
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2020-12-21 18:20:31 -0500

    Finished footer
    Imprint buttons and fixed refresh button idea for feed. created an object in php to work between views with given data.

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php 
+$user = (object) [
+    'name' => 'Daiyaan Ijaz',
+    'email' => 'daiyaanijaz@gmail.com',
+    'logged' => true
+];
+
+?>
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Mockup</title>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    </head>
+
+
+<body>
+<!-- component -->
+
+
+<!-- Footer -->
+  <div class="bg-white border-t">
+    <div class="container mx-auto px-4">
+      <div class="md:flex justify-between items-center text-sm">
+        <div class="text-center md:text-left py-3 py-4 border-b md:border-b-0">
+		<div class="text-gray-500 text-center md:mr-4">&copy; 2020 Postogon</div>
+        </div>
+				        <div class="text-center md:text-left py-3 py-4 border-b md:border-b-0">
+				<a href="#" class="inline-block leading-tight bg-blue-500 border hover:bg-blue-400 px-3 py-2 text-white rounded">Refresh</a>
+        </div>
+        <div class="md:flex md:flex-row-reverse items-center py-4">
+          <div class="text-center mb-4 md:mb-0 md:flex">
+            <div class="w-48 inline-block relative mb-4 md:mb-0 md:mr-4">
+		  <a href="#" class="no-underline text-gray-500 mr-4">Jobs</a>
+		  <a href="#" class="no-underline text-gray-500 mr-4">Legal</a>
+		  <a href="#" class="no-underline text-gray-500 mr-4">Privacy</a>
+            </div>
+            <div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
